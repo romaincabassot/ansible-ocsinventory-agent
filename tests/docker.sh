7e487ae438b9191221d4a8d1932e12af42d2f6ab
Romain CABASSOT <romain.cabassot@magellium.fr>
2016-12-16 17:04:36 +0100

    Gitlab CI modification

--- tests/docker.sh
+++ tests/docker.sh
@@ -11,4 +11,4 @@ ansible_version=$5
 container_id="$(mktemp)"
 echo "Distro=${distro} DistroVersion=${distro_version} Init=${init} run_opts=${run_opts} AnsibleVersion=${ansible_version} container_id=${container_id}"
 docker run --detach --volume="${CI_PROJECT_DIR}":/etc/ansible/roles/romaincabassot.ansible-ocsinventory-agent:ro ${run_opts} ${distro}:${distro_version} "${init}" > "${container_id}"
-docker top "${container_id}"
+docker top "$(cat ${container_id})"
