f82fb5719340fc158d58056ad5fc38a0456de1af
Romain CABASSOT <romain.cabassot@magellium.fr>
2016-12-30 14:37:36 +0100

    Ubuntu CI integration

--- tests/docker.sh
+++ tests/docker.sh
@@ -1,27 +1,55 @@
 #!/bin/bash
 
 set -e
+set -o xtrace
 
 distro=$1
 distro_version=$2
 init=$3
 run_opts=$4
 ansible_version=$5
+local_role_path=$6
 
 echo "Distro=${distro} DistroVersion=${distro_version} Init=${init} run_opts=${run_opts} AnsibleVersion=${ansible_version} container_id=${container_id}"
 
+case "${distro}" in
+    centos)
+        distro_family="redhat"
+        ;;
+    redhat)
+        distro_family="redhat"
+        ;;
+    ubuntu)
+        distro_family="debian"
+        ;;
+    debian)
+        distro_family="debian"
+        ;;
+     *)
+        echo "Unknown distro '${distro}'"
+        exit 1
+esac
+
+
 container_id="$(mktemp)"
-docker run --detach --volume="${CI_PROJECT_DIR}":/etc/ansible/roles/romaincabassot.ansible-ocsinventory-agent:ro ${run_opts} ${distro}:${distro_version} "${init}" > "${container_id}"
+docker run --detach --volume="${local_role_path}":/etc/ansible/roles/romaincabassot.ansible-ocsinventory-agent:ro ${run_opts} ${distro}:${distro_version} "${init}" > "${container_id}"
 
 # Install EPEL repository for pip package installation
-if [ "${distro}" == "centos" ] && [ "${distro_version}" == "6" ]; then docker exec --tty "$(cat ${container_id})" env TERM=xterm rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm; fi
-if [ "${distro}" == "centos" ] && [ "${distro_version}" == "7" ]; then docker exec --tty "$(cat ${container_id})" env TERM=xterm rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm; fi
+if [ "${distro_family}" == "redhat" ] && [ "${distro_version}" == "6" ]; then docker exec --tty "$(cat ${container_id})" env TERM=xterm rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm; fi
+if [ "${distro_family}" == "redhat" ] && [ "${distro_version}" == "7" ]; then docker exec --tty "$(cat ${container_id})" env TERM=xterm rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm; fi
 
 # Ansible installation via pip
-docker exec --tty "$(cat ${container_id})" env TERM=xterm yum -y install gcc gmp-devel python-devel openssl-devel findutils
-docker exec --tty "$(cat ${container_id})" env TERM=xterm yum -y install python-pip
+if [ "${distro_family}" == "redhat" ]; then
+    docker exec --tty "$(cat ${container_id})" env TERM=xterm yum -y install gcc gmp-devel python-devel openssl-devel findutils python-pip
+elif [ "${distro_family}" == "debian" ]; then
+    docker exec --tty "$(cat ${container_id})" env TERM=xterm apt-get update
+    docker exec --tty "$(cat ${container_id})" env TERM=xterm apt-get -y install python-pip python-dev findutils libssl-dev libxml2-utils cron
+    docker exec --tty "$(cat ${container_id})" env TERM=xterm pip install markupsafe
+fi
 docker exec --tty "$(cat ${container_id})" env TERM=xterm pip install --upgrade pip
-if [ "${distro}" == "centos" ] && [ "${distro_version}" == "6" ]; then docker exec --tty "$(cat ${container_id})" env TERM=xterm pip install pyopenssl; fi
+if [ "${distro_family}" == "redhat" ] && [ "${distro_version}" == "6" ]; then
+    docker exec --tty "$(cat ${container_id})" env TERM=xterm pip install pyopenssl
+fi
 docker exec --tty "$(cat ${container_id})" env TERM=xterm pip install setuptools --upgrade
 docker exec --tty "$(cat ${container_id})" env TERM=xterm pip install ansible==${ansible_version}
 
@@ -43,12 +71,5 @@ tail ${idempotence} \
 && (echo 'Idempotence test: pass' && exit 0) \
 || (echo 'Idempotence test: fail' && exit 1)
 
-
-docker exec --tty "$(cat ${container_id})" env TERM=xterm /usr/sbin/ocsinventory-agent --stdout
-docker exec --tty "$(cat ${container_id})" env TERM=xterm /usr/sbin/ocsinventory-agent --local=/var/lib/ocsinventory-agent
-
-# Debug OCS inventory log if file exist
-echo "ocsinventory-agent.log:"; if [ -f /var/log/ocsinventory-agent/ocsinventory-agent.log ]; then cat /var/log/ocsinventory-agent/ocsinventory-agent.log; fi
-
-# Run tests in Container
-docker exec --tty "$(cat ${container_id})" env TERM=xterm /bin/bash /etc/ansible/roles/romaincabassot.ansible-ocsinventory-agent/tests/test_inside_docker.sh
+# Post install tests
+docker exec --tty "$(cat ${container_id})" env TERM=xterm /bin/bash /etc/ansible/roles/romaincabassot.ansible-ocsinventory-agent/tests/test_inside_docker.sh "${distro_family}"
